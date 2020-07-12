2df5679c620dc2d003a5b60c7888af05f87ebebf
jaynamm <polky0421@gmail.com>
2020-07-12 19:02:47 +0900

    200712 update

---
+++ syntax/boolean.js
@@ -0,0 +1,6 @@
+console.log(true);
+console.log(false);
+
+var args = process.argv;
+
+console.log(args[2]);
---
+++ syntax/template.js
@@ -0,0 +1,12 @@
+var name = 'jaynam';
+var letter = 'Dear\n' + name + 'afdafssjkla;sjkal;fjklfjsdkl;ajkl;sjfdkls;fjaskla;jfdsa;dajfdkl'+name;
+
+
+console.log(letter);
+
+var letter = `Dear 
+
+
+${name} afdafssjkla;sjkal;fjklfjsdkl;ajkl;sjfdkls;fjaskla;jfdsa;dajfdkl ${name}`;
+
+console.log(letter);
